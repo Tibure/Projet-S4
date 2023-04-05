37c59044a90f7de1245cc27270917be286c1f5a4
Gabriel Rioux <riog1304@usherbrooke.ca>
2023-04-05 10:27:13 -0400

    correction bug jeux

---
+++ PPU/s4InfoAtelier4/project/src/Position.h
@@ -0,0 +1,9 @@
+#ifndef __POSITION_H__
+#define __POSITION_H__
+
+typedef struct Position {
+    double x;
+    double y;
+} Position;
+
+#endif
---
+++ PPU/s4InfoAtelier4/project/src/direction.h
@@ -0,0 +1,12 @@
+#ifndef __DIRECTION_H__
+#define __DIRECTION_H__
+
+#define DOWN 0
+#define RIGHT 1
+#define UP 2
+#define LEFT 3
+
+#define Direction int
+
+
+#endif
